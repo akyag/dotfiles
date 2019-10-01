caa09df871cec9d91910b518f9b400525542b673
akyag <akshay.226@gmail.com>
2019-10-01 17:09:50 +0530

    conky config.

---
+++ launch_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sleep 20
+conky -dq -c INSERT_PATH/time_rc
+conky -dq -c INSERT_PATH/system_rc
+conky -dq -c INSERT_PATH/processes_rc
+conky -dq -c INSERT_PATH/memory_rc
+conky -dq -c INSERT_PATH/filesystem_rc
+conky -dq -c INSERT_PATH/network_rc
+conky -dq -c INSERT_PATH/servers_rc
+conky -dq -c INSERT_PATH/mediaplayer_rc
