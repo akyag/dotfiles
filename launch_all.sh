f314abd756fa857c62c0f50bffbd4991f3e802f6
akyag <akshay.226@gmail.com>
2020-08-31 08:52:17 +0530

    Updates.

--- launch_all.sh
+++ launch_all.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
+
 sleep 20
-conky -dq -c INSERT_PATH/time_rc
-conky -dq -c INSERT_PATH/system_rc
-conky -dq -c INSERT_PATH/processes_rc
-conky -dq -c INSERT_PATH/memory_rc
-conky -dq -c INSERT_PATH/filesystem_rc
-conky -dq -c INSERT_PATH/network_rc
-conky -dq -c INSERT_PATH/servers_rc
-conky -dq -c INSERT_PATH/mediaplayer_rc
+conky -dq -c ~/.conky/cards/time_rc
+conky -dq -c ~/.conky/cards/system_rc
+conky -dq -c ~/.conky/cards/processes_rc
+conky -dq -c ~/.conky/cards/memory_rc
+conky -dq -c ~/.conky/cards/filesystem_rc
+conky -dq -c ~/.conky/cards/network_rc
+conky -dq -c ~/.conky/cards/servers_rc
+conky -dq -c ~/.conky/cards/news_rc
+# gsettings reset org.gnome.desktop.wm.keybindings switch-applications
+# gsettings reset org.gnome.desktop.wm.keybindings switch-applications-backward
+# gsettings reset org.gnome.desktop.wm.keybindings switch-group
+# gsettings reset org.gnome.desktop.wm.keybindings switch-group-backward
